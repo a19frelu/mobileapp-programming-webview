1f096e1d946ece6de81635d8c89fbf4ab92240ac
a19frelu <fredrik.lundgren93@gmail.com>
2020-04-12 16:26:24 +0200

    Ändrade public webview class till private class.

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -2,20 +2,16 @@ package com.example.webviewapp;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebView;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    public WebView webView;
+    private WebView webView;
     public void showExternalWebPage(){
         // TODO: Add your code for showing external web page here
     }
